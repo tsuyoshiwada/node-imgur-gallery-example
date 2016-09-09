7913f06f3b291227d9287a64d7680f827938404f
tsuyoshiwada <v23.b.wada@gmail.com>
2016-09-09 13:26:59 +0900

    wip

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,30 @@
 "use strict";
 
 const Router = require("express").Router;
+const multer = require("multer");
+const imgur = require("imgur");
+
 const router = Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 
 router.get("/", (req, res) => {
   res.send("ok");
+});
+
+
+router.post("/", upload.single("file"), (req, res) => {
+  const { file } = req;
+  const base64 = file.buffer.toString("base64");
+
+  imgur.uploadBase64(base64)
+    .then(json => {
+      res.json(json);
+    })
+    .catch(err => {
+      console.error(err); // eslint-disable-line no-console
+    });
 });
 
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,46 @@
 import React from "react";
 import { render } from "react-dom";
+import autobind from "autobind-decorator";
 
-const App = () => <div>"Hello World!!"</div>;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  @autobind
+  handleClick() {
+    const { files } = this.refs.file;
+    if (!files) return;
+
+    const [file] = files;
+    if (!file) return;
+
+    const data = new FormData();
+    data.append("file", file);
+
+    fetch("/api/", {
+      method: "POST",
+      body: data
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res); // TODO
+      })
+      .catch(console.error); // eslint-disable-line no-console
+  }
+
+  render() {
+    return (
+      <div>
+        <input ref="file" type="file" />
+        <button onClick={this.handleClick}>Submit</button>
+      </div>
+    );
+  }
+}
+
 
 render(
   <App />,
